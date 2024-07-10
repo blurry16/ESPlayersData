5a86c28d41948815a521d4b77a37433d785e01b5
blurry16 <roma.reznik@gmail.com>
2024-07-11 02:45:35 +0300

    Revert "now it's officially madness idk"
    This reverts commit cd57775a334b7e7c0bddf373aa41d8375a905161.

--- main.py
+++ main.py
@@ -2,22 +2,12 @@ from mojang import API
 from colorama import Back, Fore, init
 from typing import Union
 from sys import argv
-from disnake.ext import commands, tasks
-import disnake
+import os
 import requests
 import json
 import time
-import os
-
 
 ESPLAYERSDATAPATH = r"C:\Development\Python3\ESPlayersData\es_players_data.json"
-
-TOKEN = ""
-JSONRAWURL = "https://raw.githubusercontent.com/blurry16/ESPlayersData/main/uuids.json"
-COUNTCHANNELID = 1254552599210885274
-MEMBERLISTID = 1254554188461903882
-MEMBERLISTMESSAGEID = 1254556785386328136
-ROLEID = 1254744804110241822
 
 
 class Jsonfile:
@@ -35,9 +25,6 @@ class Jsonfile:
         """dumps selected data to the file"""
         with open(self.file_path, "w", encoding="UTF-8") as data_file:
             json.dump(_data, data_file, indent=indent)
-
-
-updates = 0
 
 
 esplayersdata = Jsonfile(ESPLAYERSDATAPATH)
@@ -94,58 +81,3 @@ if __name__ == "__main__":
         os.system(
             f'cd {os.curdir} && git add {ESPLAYERSDATAPATH} && git commit -m "es_players_data.json update" && git push'
         )
-
-    if "--bot" in argv or "--discord" in argv or "--discord-bot" in argv:
-        activity = disnake.Activity(
-            name="check bio",
-            type=disnake.ActivityType.playing,
-        )
-        bot = commands.Bot(
-            command_prefix="..",
-            intents=disnake.Intents.all(),
-            status=disnake.Status.online,
-            activity=activity,
-        )
-
-        @bot.event
-        async def on_ready():
-            print(f"{Fore.MAGENTA}Bot {bot.user} is ready!")
-            update_task.start()
-
-        @bot.slash_command(description="Force an update.", dm_permission=False)
-        async def update(ctx):
-            if ctx.author.get_role(ROLEID):
-                print(
-                    f"{Fore.MAGENTA}{ctx.author} forced an update at {int(time.time())}."
-                )
-                await ctx.send("Update was forced successfully!", ephemeral=True)
-                await update_data()
-            else:
-                await ctx.send("Not enough permissions.", ephemeral=True)
-
-        async def update_data():
-            try:
-                global updates
-                uuids = json.loads(requests.get(JSONRAWURL).text)
-                await bot.get_channel(COUNTCHANNELID).edit(
-                    name=f"{len(uuids)} players in ES"
-                )
-                names = [mapi.get_username(i) for i in uuids]
-                tojoin = "\n".join(sorted(names))
-                await bot.get_channel(MEMBERLISTID).get_partial_message(
-                    MEMBERLISTMESSAGEID
-                ).edit(
-                    content=f"```\n{tojoin}\n```\nUpdated <t:{int(time.time())}:R> (<t:{int(time.time())}:f>)\n[GitHub repo](https://github.com/blurry16/ESPlayersData)"
-                )
-                updates += 1
-                print(
-                    f"{Fore.GREEN}Data successfully updated at {int(time.time())}. In sum {updates} update{'s' if updates > 1 else ''} ha{'ve' if updates > 1 else 's'} taken place."
-                )
-            except Exception as e:
-                print(f"{Fore.RED}Exception {e} occurred.")
-
-        @tasks.loop(minutes=15)
-        async def update_task():
-            await update_data()
-
-        bot.run(TOKEN)
